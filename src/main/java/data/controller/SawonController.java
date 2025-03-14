2759b26f24ec93ad104b56ddf94aba49c9ca26d0
leejaeho-17 <woghdl99@naver.com>
2025-03-14 11:50:45 +0900

    sawonlist 출

--- src/main/java/data/controller/SawonController.java
+++ src/main/java/data/controller/SawonController.java
@@ -1,10 +1,30 @@
 package data.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import data.dto.SawonDto;
+import data.service.SawonService;
+import lombok.RequiredArgsConstructor;
+import naver.storage.NcpObjectStorageService;
 
 @Controller
+@RequiredArgsConstructor
+
 public class SawonController {
+	final SawonService sawonService;
+	final NcpObjectStorageService storageService;
+	
+	private String imagePath = "https://kr.object.ncloudstorage.com/bitcamp-bucket-121/sawon/";
+	private String bucketName = "bitcamp-bucket-121";
+	
 	@GetMapping("/")
 	public String mainPage()
 	{
@@ -12,9 +32,28 @@ public class SawonController {
 	}
 	
 	@GetMapping({"/list"})
-	public String sawonList()
+	public String sawonList(Model model)
 	{
+		List<SawonDto> list = sawonService.getSelectAllSawon();
+		model.addAttribute("list", list);
+		model.addAttribute("totalCount", list.size());
+		model.addAttribute("imagePath", imagePath);
+		
 		return "sawon/sawonlist";		
+	}
+	
+	@PostMapping("/insert")
+	public String sawonInsert(@ModelAttribute SawonDto dto,
+			@RequestParam("upload") MultipartFile upload) {
+		if(upload.getOriginalFilename().equals(""))
+			dto.setPhoto(null);
+		else {
+			String photo = storageService.uploadFile(bucketName, "sawon", upload);
+			dto.setPhoto(photo);
+		}
+		
+		sawonService.insertSawon(dto);
+		return "redirect:./list";
 	}
 	
 	@GetMapping("/form")
